5f35d7163826f65dfbb9c3e6e32b5d2fbf5fad32
Danusia <danuta.pezda@gmail.com>
2021-09-19 11:59:23 +0300

    web scraping puppetier

---
+++ webScrapingPPT/index.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://en.wikipedia.org/wiki/Coronavirus');
+  await page.screenshot({path: 'wiki.png'});
+
+  const result = await page.evaluate(()  => {
+    let headingFromWeb = document.querySelectorAll(".mw-headline");
+    const headingsList = [...headingFromWeb]
+    return headingsList.map(h => h.innerText);
+  });
+
+  console.log(result);
+  
+  await browser.close();
+})();
